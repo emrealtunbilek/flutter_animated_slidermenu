79633d2e37dda1548e32509c4be497cf78062c43
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-06-06 19:14:14 +0300

    bölüm 2 animatedpositioned ve systemchrome kullanımı gösterildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'menu_dashboard_widget.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  final Color backgroundColur = Color(0xFF343442);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: backgroundColur,
+    statusBarIconBrightness: Brightness.light,
+    //systemNavigationBarColor: Colors.pink,
+    //systemNavigationBarIconBrightness: Brightness.dark,
+  ));
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(MyApp());
+  });
+}
 
 class MyApp extends StatelessWidget {
   @override
--- lib/menu_dashboard_widget.dart
+++ lib/menu_dashboard_widget.dart
@@ -3,9 +3,20 @@ import 'package:flutter/material.dart';
 final TextStyle menuFontStyle = TextStyle(color: Colors.white, fontSize: 20);
 final Color backgroundColur = Color(0xFF343442);
 
-class MenuDashboard extends StatelessWidget {
+class MenuDashboard extends StatefulWidget {
+  @override
+  _MenuDashboardState createState() => _MenuDashboardState();
+}
+
+class _MenuDashboardState extends State<MenuDashboard> {
+  double ekranYuksekligi, ekranGenisligi;
+  bool menuAcikMi = false;
+
   @override
   Widget build(BuildContext context) {
+    ekranYuksekligi = MediaQuery.of(context).size.height;
+    ekranGenisligi = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: backgroundColur,
       body: SafeArea(
@@ -71,32 +82,47 @@ class MenuDashboard extends StatelessWidget {
   }
 
   Widget dashBoardOlustur(BuildContext context) {
-    return Material(
-      elevation: 8,
-      color: backgroundColur,
-      child: Container(
-        padding: EdgeInsets.only(left: 16, right: 16, top: 8),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Icon(
-                  Icons.menu,
-                  color: Colors.white,
-                ),
-                Text(
-                  "My Cards",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                ),
-                Icon(
-                  Icons.add_circle_outline,
-                  color: Colors.white,
-                ),
-              ],
-            )
-          ],
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 500),
+      top: menuAcikMi ? 0.2 * ekranYuksekligi : 0,
+      bottom: menuAcikMi ? 0.2 * ekranYuksekligi : 0,
+      left: menuAcikMi ? 0.4 * ekranGenisligi : 0,
+      right: menuAcikMi ? -0.4 * ekranGenisligi : 0,
+      child: Material(
+        borderRadius: menuAcikMi ? BorderRadius.all(Radius.circular(40)) : null,
+        elevation: 8,
+        color: backgroundColur,
+        child: Container(
+          padding: EdgeInsets.only(left: 16, right: 16, top: 8),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        menuAcikMi = !menuAcikMi;
+                      });
+                    },
+                    child: Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "My Cards",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  Icon(
+                    Icons.add_circle_outline,
+                    color: Colors.white,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
